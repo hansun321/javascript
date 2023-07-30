a85b28e863d550768186b03c17d1aa4c075d6a08
hansun321 <hansun321@gmail.com>
2023-07-31 06:22:03 +0900

    player

--- vdo/vdo.js
+++ vdo/vdo.js
@@ -1,69 +1,42 @@
 /* 버튼 선택자 */
-// var video = ;
-// var restart = ;
-// var play = ;
-// var pause = ;
-// var stop = ;
-// var timeCurrent = ;
-// var rew = ;
-// var fastFwd = ;
-// var mute = ;
-// var unmute = ;
-// var vol = ;
-// var vmup = ;
-// var vmdown = ;
-// var full = ;
-// var sizeup = ;
-// var sizedown = ;
-// var progress = ;
-// var progressBar = ;
-// var volTxt = ; 
-video.load();  //비디오 로딩
-video.setAttribute("loop","5"); //비디오 반복 횟수 지정
-video.volume = 1;   //비디오의 볼륨값 초기화
-
+var video = document.getElementById("video");
+var restart = document.getElementById("restart");
+var play = document.getElementById("play");
+var pause = document.getElementById("pause");
+var stop = document.getElementById("stop");
+var timeCurrent = document.getElementById("currentTime");
+var rew = document.getElementById("rew");
+var fastFwd = document.getElementById("fastFwd");
+var mute = document.getElementById("mute");
+var unmute = document.getElementById("unmute");
+var vol = document.getElementById("vol");
+var vmup = document.getElementById("vmup");
+var vmdown = document.getElementById("vmdown");
+var full = document.getElementById("full");
+var sizeup = document.getElementById("sizeup");
+var sizedown = document.getElementById("sizedown");
+var progress = document.getElementById("progress");
+var progressBar = document.getElementById("progressBar");
+var volTxt = document.getElementById("volTxt"); 
+video.load();  
+video.setAttribute("loop","5");
+video.volume = 1;
 /* 동영상 재생 제어 처리부 */
-//재시작
-restart.addEventListener("click", function () {        });
-//10초 뒤로 가기
-rew.addEventListener("click", function(){        skip(-10);    });
-//10초 앞으로 가기  
-fastFwd.addEventListener("click", function(){        skip(10);    });  
-//재생 함수 호출
-play.addEventListener("click", function () {            });
-//일시정지 함수 호출
-pause.addEventListener("click", function () {           });
-//정지 함수 호출
-stop.addEventListener("click", function () {          });
-//음소거 함수 호출
-mute.addEventListener("click", function () {        });
-//음삽입 함수 호출
-unmute.addEventListener("click", function () {        });
-//볼륨 조절바 기능 정의 및 호출
-vol.addEventListener("change", function(){  
-    video.volume = this.value/1000;
-    volTxt.innerText = parseInt(video.volume*100)+"%";
-});
-vmup.addEventListener("click", function () {        volumeUpDown("up");    });//볼륨업
-vmdown.addEventListener("click", function () {        volumeUpDown("down");    });//볼륨다운
-full.addEventListener("click", function () {        fullScreen();    });//풀스크린
-var w=640;
-video.width=w;
-sizeup.addEventListener("click", function(){    video.width += 200;    }); //사이즈업
-sizedown.addEventListener("click", function(){  video.width -= 200;    }); //사이즈 다운
-
-//처음부터 다시 재생
-function restartPlay() {   }
-//앞으로, 뒤로 가기 함수 
-function skip(value) {  } 
-//재생일시정지 함수
-function playPause () {  
+function restartPlay() { video.currentTime = 0;   video.play();  } //처음부터 다시 재생
+function skip(value) { video.currentTime += value; } //10초 앞으로, 10초 뒤로 가기 함수
+restart.addEventListener("click", function () {        restartPlay();    });//재시작
+rew.addEventListener("click", function(){        skip(-10);    });  //뒤로 가기
+fastFwd.addEventListener("click", function(){        skip(10);    });  //앞으로 가기  
+function playPause () {  //재생일시정지 함수
     if (video.paused) { video.play(); play.style.display = "none"; pause.style.display = "inline-block";
     } else { video.pause(); play.style.display = "inline-block";   pause.style.display = "none";
     }
 }   
-//정지함수
-function stopPlayer () { video.pause(); video.currentTime = 0; } 
+function stopPlayer () { video.pause(); video.currentTime = 0; } //정지함수
+play.addEventListener("click", function () {        playPause();    });//재생
+pause.addEventListener("click", function () {        playPause();    });//일시정지
+stop.addEventListener("click", function () {        stopPlayer();    });//정지
+
 /* 음량 제어 처리부 */
 function mutePlayer (state) { //음소거토글함수
     if (state == "mute") { video.volume = 0;  mute.style.display = "none";
@@ -86,6 +59,14 @@ function volumeUpDown (dir) { //볼륨updown
     }
     volTxt.innerText = parseInt(video.volume*100)+"%";
 }
+mute.addEventListener("click", function () {        mutePlayer("mute")    });//음소거
+unmute.addEventListener("click", function () {        mutePlayer("unmute") })//음삽입
+vol.addEventListener("change", function(){  //볼륨 조절바
+    video.volume = this.value/1000;
+    volTxt.innerText = parseInt(video.volume*100)+"%";
+});
+vmup.addEventListener("click", function () {        volumeUpDown("up");    });//볼륨업
+vmdown.addEventListener("click", function () {        volumeUpDown("down");    });//볼륨다운
 
 //동영상 크기제어 처리부
 function fullScreen () { //전체화면 만들기
@@ -94,6 +75,11 @@ function fullScreen () { //전체화면 만들기
     else if (video.webkitRequestFullscreen) { video.webkitRequestFullscreen(); } 
     else if (video.msRequestFullscreen) { video.msRequestFullscreen(); }
 }
+full.addEventListener("click", function () {        fullScreen();    });//풀스크린
+var w=640;
+video.width=w;
+sizeup.addEventListener("click", function(){    video.width += 200;    }); //사이즈업
+sizedown.addEventListener("click", function(){  video.width -= 200;    }); //사이즈 다운
 
 //재생시간 표시 처리부
 playTime();  //재생시간 표시 함수
